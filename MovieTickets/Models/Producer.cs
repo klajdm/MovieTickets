3d3862377f354a63e331f724e54a16b2a3074e97
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-04 23:36:09 +0200

    added initial models

---
+++ MovieTickets/Models/Producer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieTickets.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ProfilePictureUrl { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+    }
+}
