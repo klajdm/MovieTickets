a6181fd714051cef9feb2004d2c391f26a09ca0e
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-06 01:23:10 +0200

    implemented Entity base repository

---
+++ MovieTickets/Data/Base/IEntityBase.cs
@@ -0,0 +1,7 @@
+﻿namespace MovieTickets.Data.Base
+{
+    public interface IEntityBase
+    {
+        int Id { get; set; }
+    }
+}
--- MovieTickets/Data/Services/IActorsService.cs
+++ MovieTickets/Data/Services/IActorsService.cs
@@ -1,13 +1,9 @@
-﻿using MovieTickets.Models;
+﻿using MovieTickets.Data.Base;
+using MovieTickets.Models;
 
 namespace MovieTickets.Data.Services
 {
-    public interface IActorsService
+    public interface IActorsService:IEntityBaseRepository<Actor>
     {
-        Task<IEnumerable<Actor>> GetAllAsync();
-        Task<Actor> GetByIdAsync(int id);
-        Task AddAsync(Actor actor);
-        Task<Actor> UpdateAsync(int id, Actor newActor);
-        Task DeleteAsync(int id);
     }
 }
