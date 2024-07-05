d39d36fee03237d73b5512299b672dc904054b18
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-06 00:23:13 +0200

    add actors details view

--- MovieTickets/Controllers/ActorsController.cs
+++ MovieTickets/Controllers/ActorsController.cs
@@ -15,7 +15,7 @@ namespace MovieTickets.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var data = await _service.GetAll();
+            var data = await _service.GetAllAsync();
             return View(data);
         }
 
@@ -33,8 +33,17 @@ namespace MovieTickets.Controllers
                 return View(actor);
             }
 
-            _service.Add(actor);
+            await _service.AddAsync(actor);
             return RedirectToAction(nameof(Index)); 
+        }
+
+        //Get: Actors/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var actorsDetails = await _service.GetByIdAsync(id);
+
+            if(actorsDetails == null) return View("empty");
+            return View(actorsDetails);
         }
     }
 }
