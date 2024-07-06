93c544edd102adfb01bc16093e00f2731d43e0ce
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-06 23:48:11 +0200

    added orders controller

---
+++ MovieTickets/Controllers/OrdersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieTickets.Data.Cart;
+using MovieTickets.Data.Services;
+using MovieTickets.Data.ViewModels;
+
+namespace MovieTickets.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IMoviesService _moviesService;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrdersController(IMoviesService moviesService, ShoppingCart shoppingCart)
+        {
+            _moviesService = moviesService;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var response = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(response);
+        }
+    }
+}
---
+++ MovieTickets/Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,10 @@
+﻿using MovieTickets.Data.Cart;
+
+namespace MovieTickets.Data.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public double ShoppingCartTotal { get; set; }
+    }
+}
