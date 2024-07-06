2de2613d740c8bfa792a737d2a1ba449a95ea567
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-07 00:57:53 +0200

    added orders ViewComponent

---
+++ MovieTickets/Data/ViewComponent/ShoppingCartSummary.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieTickets.Data.Cart;
+
+namespace MovieTickets.Data.ViewComponents
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            var count = items.Count;
+
+            return View(count);
+        }
+    }
+}
