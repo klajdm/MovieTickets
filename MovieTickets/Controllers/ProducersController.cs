a8132fefd255e725322fc96af414fbcbf6d3c1e0
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-06 10:47:25 +0200

    add a new producer

--- MovieTickets/Controllers/ProducersController.cs
+++ MovieTickets/Controllers/ProducersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using MovieTickets.Data;
 using MovieTickets.Data.Services;
+using MovieTickets.Models;
 
 namespace MovieTickets.Controllers
 {
@@ -18,13 +19,29 @@ namespace MovieTickets.Controllers
             return View(data);
         }
 
-        //Get: Actors/Details/1
+        //Get: Producers/Details/1
         public async Task<IActionResult> Details(int id)
         {
             var producersDetails = await _service.GetByIdAsync(id);
 
             if (producersDetails == null) return View("NotFound");
             return View(producersDetails);
+        }
+
+        //Get: Producers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("FullName,ProfilePictureUrl,Bio")] Producer producer)
+        {
+            if (!ModelState.IsValid) return View(producer);
+           
+
+            await _service.AddAsync(producer);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
