3816d724efdedd57326513c19c4287317833ffcc
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-07 00:28:04 +0200

    add, remove item from cart

--- MovieTickets/Controllers/OrdersController.cs
+++ MovieTickets/Controllers/OrdersController.cs
@@ -29,5 +29,27 @@ namespace MovieTickets.Controllers
 
             return View(response);
         }
+
+        public async Task<IActionResult> AddItemToShoppingCart(int id)
+        {
+            var item = await _moviesService.GetMovieByIdAsync(id);
+
+            if (item != null)
+            {
+                _shoppingCart.AddItemToCart(item);
+            }
+            return RedirectToAction(nameof(ShoppingCart));
+        }
+
+        public async Task<IActionResult> RemoveItemFromShoppingCart(int id)
+        {
+            var item = await _moviesService.GetMovieByIdAsync(id);
+
+            if (item != null)
+            {
+                _shoppingCart.RemoveItemFromCart(item);
+            }
+            return RedirectToAction(nameof(ShoppingCart));
+        }
     }
 }
