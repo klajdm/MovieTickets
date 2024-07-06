f83ae718e36339f300aa1c95c477b8f628f0bbe8
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-06 18:12:38 +0200

    added shopping cart model

--- MovieTickets/Data/AppDbContext.cs
+++ MovieTickets/Data/AppDbContext.cs
@@ -37,5 +37,6 @@ namespace MovieTickets.Data
         //Orders related tables
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
     }
 }
--- MovieTickets/Migrations/20240706161108_ShoppingCartItems_Added.Designer.cs
+++ MovieTickets/Migrations/20240706161108_ShoppingCartItems_Added.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieTickets.Data;
 
@@ -11,9 +12,11 @@ using MovieTickets.Data;
 namespace MovieTickets.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240706161108_ShoppingCartItems_Added")]
+    partial class ShoppingCartItems_Added
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
