929cde0f9f780d0f06e4cf13be77c9082e162c42
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-07 18:21:34 +0200

    register new user

--- MovieTickets/Controllers/AccountController.cs
+++ MovieTickets/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MovieTickets.Data;
+using MovieTickets.Data.Static;
 using MovieTickets.Data.ViewModels;
 using MovieTickets.Models;
+using System.Linq.Expressions;
 
 namespace MovieTickets.Controllers
 {
@@ -47,5 +49,32 @@ namespace MovieTickets.Controllers
         }
 
         public IActionResult Register() => View(new RegisterVM());
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid) return View(registerVM);
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+
+            if (user != null)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerVM);
+            }
+
+            var newUser = new ApplicationUser()
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if (newUserResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+
+            return View("RegisterCompleted");
+        }
     }
 }
