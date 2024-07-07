0863ea031da652189bf43c55c3bfcdb2f2bd1a0e
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-07 19:20:10 +0200

    orders final touches

--- MovieTickets/Controllers/OrdersController.cs
+++ MovieTickets/Controllers/OrdersController.cs
@@ -2,6 +2,7 @@
 using MovieTickets.Data.Cart;
 using MovieTickets.Data.Services;
 using MovieTickets.Data.ViewModels;
+using System.Security.Claims;
 
 namespace MovieTickets.Controllers
 {
@@ -20,9 +21,10 @@ namespace MovieTickets.Controllers
 
         public async Task<IActionResult> Index()
         {
-            string userId = "";
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            string userRole = User.FindFirstValue(ClaimTypes.Role);
 
-            var orders = await _ordersService.GetOrdersByUserIdAsync(userId);
+            var orders = await _ordersService.GetOrdersByUserIdAndRoleAsync(userId, userRole);
             return View(orders);
         }
 
@@ -66,8 +68,8 @@ namespace MovieTickets.Controllers
         public async Task<IActionResult> CompleteOrder()
         {
             var items = _shoppingCart.GetShoppingCartItems();
-            string userId = "";
-            string userEmailAddress = "";
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            string userEmailAddress = User.FindFirstValue(ClaimTypes.Email);
 
             await _ordersService.StoreOrderAsync(items, userId, userEmailAddress);
             await _shoppingCart.ClearShoppingCartAsync();
